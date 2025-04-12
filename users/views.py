481b5ac86b26122bfcb9c9db57b51aab256a9e91
Marcus Martins <marcusmartins38@gmail.com>
2025-04-12 00:24:24 -0400

    feat: user register view & url

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 
-# Create your views here.
+from users.serializers import RegisterSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
