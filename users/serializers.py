30811a6f38c6a03cebce650f563f2479771c8739
Marcus Martins <marcusmartins38@gmail.com>
2025-04-13 22:17:39 -0400

    feat: update user info

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,56 @@ from rest_framework import serializers
 
 User = get_user_model()
 
+class UserSerializer(serializers.ModelSerializer):
+    current_password = serializers.CharField(write_only=True, required=False)
+    new_password = serializers.CharField(write_only=True, required=False)
+    confirm_new_password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'current_password', 'new_password', 'confirm_new_password']
+        read_only_fields = ['id']
+        extra_kwargs = {
+            'username': {'required': False},
+            'email': {'required': False}
+        }
+
+    def validate(self, attrs):
+        if 'new_password' in attrs:
+            if 'current_password' not in attrs:
+                raise serializers.ValidationError(
+                    {"current_password": "Current password is required to set a new password."})
+
+            user = self.context['request'].user
+            if not user.check_password(attrs['current_password']):
+                raise serializers.ValidationError({"current_password": "Current password is incorrect."})
+
+            if 'confirm_new_password' not in attrs:
+                raise serializers.ValidationError({"confirm_new_password": "Password confirmation is required."})
+
+            if attrs['new_password'] != attrs['confirm_new_password']:
+                raise serializers.ValidationError({"new_password": "New password and confirmation do not match."})
+
+        elif any(field in attrs for field in ['current_password', 'confirm_new_password']):
+            raise serializers.ValidationError(
+                {"new_password": "All password fields must be provided to update password."})
+
+        return attrs
+
+    def update(self, instance, validated_data):
+        current_password = validated_data.pop('current_password', None)
+        new_password = validated_data.pop('new_password', None)
+        confirm_new_password = validated_data.pop('confirm_new_password', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if new_password:
+            instance.set_password(new_password)
+
+        instance.save()
+        return instance
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password_confirm = serializers.CharField(write_only=True, required=True)
--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 
-from users.serializers import RegisterSerializer
+from users.serializers import RegisterSerializer, UserSerializer
 
 User = get_user_model()
+
+class UserMeView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
