24c4b545ec79860a84c00632d6a8d4c037181aaf
Marcus Martins <marcusmartins38@gmail.com>
2025-04-16 18:56:55 -0400

    feat: unique email

--- movies/models.py
+++ movies/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-# Create your models here.
 class Movie(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
