4b841124eabb590f4b743c1b40f621fc19bdfdd3
Marcus Martins <marcusmartins38@gmail.com>
2025-04-16 18:41:19 -0400

    fix: title ordering case insensitive

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions, filters
-
+from django.db.models.functions import Lower
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 
@@ -7,11 +7,21 @@ class MovieListCreateView(generics.ListCreateAPIView):
     serializer_class = MovieSerializer
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['title', 'rate']
+    ordering_fields = ['title', 'rate', 'created_at']
     ordering = ['-created_at']
 
     def get_queryset(self):
-        return Movie.objects.filter(user=self.request.user)
+        queryset = Movie.objects.filter(user=self.request.user)
+        return queryset
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+
+        ordering = self.request.query_params.get('ordering')
+        if ordering == 'title':
+            return queryset.order_by(Lower('title'))
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
